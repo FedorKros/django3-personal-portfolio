f9aa4864bf10f36dfce4ffe31d39f72ef9f10969
FedorKros <klimovichf@gmail.com>
2021-08-11 10:03:57 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pmdpe@i9k&zz+%cumn1a7th#loemcuhhj%n_al*5f!*ot!m-v^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fedorkros.pythonanywhere.com']
 
 
 # Application definition
